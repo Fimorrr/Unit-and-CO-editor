afa850b61aca2bb76c46270af63b87d2021a910f
fimor <fimorrr@yandex.ru>
2020-10-06 22:46:13 +0300

    Add json filling

--- src/App.js
+++ src/App.js
@@ -8,26 +8,66 @@ class App extends Component {
 
     this.state = {
       options: {
-        unitNames: ["Infantry"],
+        unitNames: [],
         unitProperties: [],
         coNames: []
-      }
+      },
+      units: {},
+      currentUnitName: "Infantry"
     };
 
     fetch('../data/options.json').then(response => {
-      console.log(response);
       return response.json();
     }).then(data => {
-      // Work with JSON data here
-      console.log(data);
       this.setState(() => ({
         options: data
       }));
+      this.initJson();
     }).catch(err => {
       // Do something for an error here
       console.log("Error Reading data " + err);
     });
 
+    this.changeJson = this.changeJson.bind(this);
+    this.changeUnitName = this.changeUnitName.bind(this);
+  }
+
+  initJson = () => {
+    this.state.options.unitNames.forEach((unitName) => {
+      this.setState(() => ({
+        units: {
+          ...this.state.units,
+          [unitName]: {}
+        }
+      }));
+    });
+  }
+
+  changeJson = (event, propertyName) => {
+    console.log("Json changed: " + propertyName + ", " + event.target.value);
+    var inputValue = event.target.value;
+
+    this.setState(() => ({
+      units: {
+        ...this.state.units,
+        [this.state.currentUnitName]: {
+          ...this.state.units.Infantry,
+          [propertyName]: inputValue
+        }
+      }
+    }));
+
+    console.log(this.state);
+  }
+
+  generateJson = () => {
+    console.log("Json generated");
+  }
+
+  changeUnitName = (unitName) => {
+    this.setState(() => ({
+      currentUnitName: unitName
+    }));
   }
 
   render() {
@@ -36,11 +76,15 @@ class App extends Component {
         <div className="App-container">
           <UnitList
             names={this.state.options.unitNames}
+            changeUnitName={this.changeUnitName}
           />
           <Editor
+            unitName={this.state.currentUnitName}
             properties={this.state.options.unitProperties}
+            changeJson={this.changeJson}
           />
         </div>
+        <button onClick={this.generateJson}>Download JSON</button>
       </div>
     );
   }
@@ -53,6 +97,7 @@ function UnitList(props) {
         {props.names.map((item, index) => (
           <ListElement
             name={item}
+            changeUnitName={props.changeUnitName}
           />
         ))}
       </ul>
@@ -64,7 +109,7 @@ function ListElement(props) {
   let imgSrc = "../resources/units/" + props.name + ".png";
 
   return (
-    <li>
+    <li className="Like-link" onClick={() => props.changeUnitName(props.name)}>
       <img src={imgSrc} width="30px" height="30px"/>
       {props.name}
     </li>
@@ -72,12 +117,17 @@ function ListElement(props) {
 }
 
 function Editor(props) {
+  let imgSrc = "../resources/units/" + props.unitName + ".png";
+
   return (
     <div className="App-container-block">
+      <div>{props.unitName}</div>
+      <img src={imgSrc} width="100px" height="100px"/>
       <ul>
         {props.properties.map((item, index) => (
           <EditorElement
             name={item.name}
+            changeJson={props.changeJson}
           />
         ))}
       </ul>
@@ -89,7 +139,7 @@ function EditorElement(props) {
   return (
     <li className="Editor-container">
       <div class="Editor-container-element">{props.name}</div>
-      <input class="Editor-container-element"/>
+      <input onChange={(event) => props.changeJson(event, props.name)} class="Editor-container-element"/>
     </li>
   );
 }
